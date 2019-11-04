292a9ccf5eebc17a7f24efda109f0c0c60764d14
Sam2471 <samf4553@gmail.com>
2019-11-04 15:25:11 +0000

    Added the most low end movement system ever. But it's a start.

---
+++ Colossal Shadow The Game/Assets/Playercontrols.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrols : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update()
+     {
+        if (Input.GetKey(KeyCode.A))
+         {
+            rb.velocity = new Vector2(-5, 0);
+         }
+        if (Input.GetKey(KeyCode.D))
+         {
+            rb.velocity = new Vector2(5, 0);
+         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, 10);
+        }
+
+
+
+
+
+
+    }
+    
+}
