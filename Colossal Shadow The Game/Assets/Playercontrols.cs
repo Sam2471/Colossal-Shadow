874094bafb592d4d53145baae51dac9215af4803
Sam2471 <samf4553@gmail.com>
2019-11-12 15:25:20 +0000

    Finished video 16

--- Colossal Shadow The Game/Assets/Playercontrols.cs
+++ Colossal Shadow The Game/Assets/Playercontrols.cs
@@ -6,13 +6,16 @@ public class Playercontrols : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    private enum State {idel, running, jumping}
+    private enum State {idel, running, jumping, falling}
     private State state = State.idel;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
     private void Update()
      {
@@ -38,7 +41,7 @@ public class Playercontrols : MonoBehaviour
         {
             
         }
-        if (vDirection > 0)
+        if (Input.GetButtonDown("Jump")&& coll.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
             state = State.jumping;
@@ -53,9 +56,18 @@ public class Playercontrols : MonoBehaviour
     {
         if(state == State.jumping)
         {
+            if (rb.velocity.y < .1f)
+                state = State.falling; 
 
         }
-        
+        else if(state == State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                state = State.idel;
+            }
+        }
+
         else if(Mathf.Abs(rb.velocity.x) > 2f)
         {
             //Moving
