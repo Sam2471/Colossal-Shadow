eca3cba4fddb70329b27b9de1bdeda068c3467e0
Sam2471 <samf4553@gmail.com>
2019-11-12 14:05:59 +0000

    Finished video 14-15. Changed the code to be "cleaner" and more complex.

--- Colossal Shadow The Game/Assets/Playercontrols.cs
+++ Colossal Shadow The Game/Assets/Playercontrols.cs
@@ -4,40 +4,69 @@ using UnityEngine;
 
 public class Playercontrols : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {idel, running, jumping}
+    private State state = State.idel;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
     private void Update()
      {
+        float hDirection = Input.GetAxis("Horizontal");
+        float vDirection = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.A))
+        if (hDirection < 0)
          {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("running", true);
+            
          }
 
-        else if (Input.GetKey(KeyCode.D))
+        else if (hDirection > 0)
          {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("running", true);
+            
 
         }
 
         else
         {
-            anim.SetBool("running", false);
+            
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (vDirection > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 15f);
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state = State.jumping;
         }
 
+        VelocityState();
+        anim.SetInteger("state", (int)state);
 
 
+    }
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
 
+        }
+        
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Moving
+            state = State.running;
+        }
 
-
+        else
+        {
+            state = State.idel;
+        }
+       
     }
     
 }
