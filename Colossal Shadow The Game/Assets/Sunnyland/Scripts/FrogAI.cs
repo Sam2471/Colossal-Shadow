df2aa3c72567830f9000a22dc0300115117db6f2
Samuel Flowers <475197@chrome.escg.ac.uk>
2019-11-14 22:31:36 +0000

    Finished Video 24 - AI

---
+++ Colossal Shadow The Game/Assets/Sunnyland/Scripts/FrogAI.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogAI : MonoBehaviour
+{
+    [SerializeField] private float leftCap;
+    [SerializeField] private float rightCap;
+
+    [SerializeField] private float jumpLength = 5f;
+    [SerializeField] private float jumpHeight = 15f;
+    [SerializeField] private LayerMask ground;
+
+
+    private Collider2D coll;
+    private Rigidbody2D rb;
+
+    private bool facingLeft = true;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        if (facingLeft)
+        {
+            // Test if frog is across left border
+            if (transform.position.x > leftCap)
+            {
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+                //test if object is touching gorund for jump purposes
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(-jumpLength, jumpHeight);
+                }
+            }
+            else
+            {
+                facingLeft = false;
+            }
+        }
+        else
+        {
+            // Test if frog is across right border
+            if (transform.position.x < rightCap)
+            {
+                if (transform.localScale.x != -1)
+                {
+                    transform.localScale = new Vector3(-1, 1);
+                }
+                //test if object is touching gorund for jump purposes
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(jumpLength, jumpHeight);
+                }
+            }
+            else
+            {
+                facingLeft = true;
+            }
+        }
+
+
+
+    }  
+}
