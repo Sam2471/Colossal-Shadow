fc801ccc3621f1098dd82faedca45e7525b604a5
Sam2471 <samf4553@gmail.com>
2019-11-26 16:27:13 +0000

    Restricted camera movent to not fall out of the map with the player. Need to fix the raycast beam with the enemy.

--- Colossal Shadow The Game/Assets/CS TG ASSETS/Scripts/BanditMovement.cs
+++ Colossal Shadow The Game/Assets/CS TG ASSETS/Scripts/BanditMovement.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class BanditMovement : MonoBehaviour
 {
-    
+    public float speedbl;
+    private float distance;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speedbl * Time.deltaTime);
+
+        RaycastHit2D groundinfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 10f);
+        if(groundinfo.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
 }
